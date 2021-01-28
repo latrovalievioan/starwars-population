1da099f67629918b4678b1e34f3543b72980cf06
Ioan Latrovaliev <latrovalievioan@gmail.com>
2021-01-28 14:33:57 +0200

    feat: implement planet population

--- src/app/utils.js
+++ src/app/utils.js
@@ -1,3 +1,7 @@
 /**
  * Here you can define helper functions to use across your app.
  */
+export default function delay(s) {
+  const ms = s * 1000;
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
